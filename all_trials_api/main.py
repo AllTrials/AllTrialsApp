7ff76ea0881d5c07a37241a6eb25e478314fcad5
wesserg <wesserg@protonmail.com>
2023-12-11 14:07:31 -0700

    Add openai text completion for aact queries example

--- all_trials_api/main.py
+++ all_trials_api/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from fastapi import FastAPI, Response
-from alltrialsapp.examples import get_studies
+from alltrialsapp.examples import get_studies, get_user_data, get_query_completion
 app = FastAPI()
 
 
@@ -9,6 +9,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+EXAMPLE_TEXT = """
+Please show me all studies that mention the ALS condition in brief summaries or study titles.
+"""
 
 # This is an example of a path parameter and query parameters
 # Try hitting http://127.0.0.1:8000/condition/?name=als&return_data=column1,column2&limit=50
@@ -50,4 +53,33 @@ async def studies(
     """
     # Return HTML content as response
     return Response(content=html_content, media_type="text/html")
-    
+
+
+@app.get("/example/")
+async def example():
+    print(EXAMPLE_TEXT)
+    aact_query = get_query_completion(EXAMPLE_TEXT)
+    print(aact_query)
+    df = get_user_data(aact_query=aact_query)
+    print(df)
+    df_html = df.to_html(classes="table table-striped table-bordered")
+    html_content = f"""
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+        </head>
+        <body>
+            <script>
+                $(document).ready(function() {{
+                    $('#myTable').DataTable();
+                }});
+            </script>
+            {df_html}
+            
+        </body>
+    </html>
+    """
+    # Return HTML content as response
+    return Response(content=html_content, media_type="text/html")
--- alltrialsapp/examples.py
+++ alltrialsapp/examples.py
@@ -6,6 +6,8 @@ from typing import Dict, Union
 import psycopg2
 import pandas as pd
 from psycopg2.extensions import connection
+import os
+from openai import OpenAI
 
 """
 Database connection parameters.
@@ -18,6 +20,54 @@ DB_PARAMS: Dict[str, Union[str, int]] = {
     'host': 'aact-db.ctti-clinicaltrials.org',
     'port': 5432  # Default PostgreSQL port
 }
+
+
+def check_aact_query(aact_query: str) -> bool:
+    """
+    This function checks if the aact_query is a valid sql query
+    to the aact database.
+    """
+
+    return True
+
+def get_query_completion(aact_query: str) -> str:
+    """ 
+    This function used a text input and openain text completion to convert
+    the text input into a query that can be used to query the aact database.
+    """    
+    prompt_prefix = """
+    You are an sql query assistant. You have deep knowledge of the aact clinical trials database.
+    You are responding to a user data request inserted in textbox on a webapp. 
+    User intends to query the AACT database. User might not know sql. 
+    Your job is to convert the  provided text to a valid sql query to the aact postgres database. 
+    At your disposal are the following tables from the aact database: 
+    [studies, conditions, brief_summaries, calculated_values]
+    If not specified differently in the text, assume the following additional information:
+    - query the studies table.
+    - query the ctgov schema.
+    - return all columns.
+    - limit the number of rows returned to 1000.
+    
+    Here is the user provided text:
+    """
+
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    aact_query_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"{prompt_prefix}{aact_query}",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    # test if the aact_query is a valid query:
+    aact_query = aact_query_completion.choices[0].message.content
+    return aact_query
 
 
 def get_aact_connection(db_params: Dict[str, Union[str, int]] = DB_PARAMS) -> connection:
@@ -39,7 +89,80 @@ def get_aact_connection(db_params: Dict[str, Union[str, int]] = DB_PARAMS) -> co
         print("Error:", error)
 
 
+def get_user_data(aact_query: str) -> pd.DataFrame:
+    """
+    Retrieves data from the AACT database for a specified table and schema.
+
+    Args:
+    - aact_table (str): The table name in the AACT database.
+    - aact_schema (str): The schema name in the AACT database.
+    - n_rows_limit (int): Maximum number of rows to fetch.
+
+    Returns:
+    - pd.DataFrame: DataFrame containing the queried data.
+    """
+    conn = get_aact_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(aact_query)
+    
+    result = cursor.fetchall()
+    conn.close()
+    
+    # Get column names
+    column_names = [desc[0] for desc in cursor.description]
+
+    # Convert the result to a DataFrame with column names
+    df = pd.DataFrame(result, columns=column_names)
+    # Additional data handling steps (commented out)
+    # if len(df) > 0 and "nct_id" in df.columns and df["nct_id"].nunique() == len(df):
+    #    df.set_index('nct_id', inplace=True)
+    # if "id" in df.columns:
+    #    df.drop('id', axis=1, inplace=True)
+    
+    return df
+
+
 def get_studies(aact_table: str = 'studies', aact_schema: str = 'ctgov', n_rows_limit: int = 10000) -> pd.DataFrame:
+    """
+    Retrieves data from the AACT database for a specified table and schema.
+
+    Args:
+    - aact_table (str): The table name in the AACT database.
+    - aact_schema (str): The schema name in the AACT database.
+    - n_rows_limit (int): Maximum number of rows to fetch.
+
+    Returns:
+    - pd.DataFrame: DataFrame containing the queried data.
+    """
+    conn = get_aact_connection()
+    cursor = conn.cursor()
+    aact_query = f"""
+    SELECT * FROM {aact_schema}.{aact_table}
+    LIMIT {n_rows_limit};
+    """
+
+    cursor.execute(aact_query)
+    
+    result = cursor.fetchall()
+    conn.close()
+    
+    # Get column names
+    column_names = [desc[0] for desc in cursor.description]
+
+    # Convert the result to a DataFrame with column names
+    df = pd.DataFrame(result, columns=column_names)
+    # Additional data handling steps (commented out)
+    # if len(df) > 0 and "nct_id" in df.columns and df["nct_id"].nunique() == len(df):
+    #    df.set_index('nct_id', inplace=True)
+    # if "id" in df.columns:
+    #    df.drop('id', axis=1, inplace=True)
+    
+    return df
+
+
+# This is a function that fetched and prints brief summaries from the table in aact database
+def get_brief_summaries(aact_table: str = 'brief_summaries', aact_schema: str = 'ctgov', n_rows_limit: int = 10000) -> pd.DataFrame:
     """
     Retrieves data from the AACT database for a specified table and schema.
 
